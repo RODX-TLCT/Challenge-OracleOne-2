3ee7a21ce7d58dfddaf578092d34d0080b46f1aa
RODX <x.vzrm.20@gmail.com>
2022-02-04 22:28:45 -0500

    imput caption setted *save into into var*

--- JS/main.js
+++ JS/main.js
@@ -1,25 +1,39 @@
-// definiendo variables y trayendo datos del html
+// capturar inputs 
+var AddWordsForm = document.querySelector(".newWordForm");
 
-var buttonAdd = document.querySelector("#buttonAddWords");
-var buttonAddleter = document.querySelector("#buttonAddleter");
+var inputAddLeter = document.querySelector("#InputAddleter");
 
-/*
-var textAddword = document.querySelector("#inputAdd");
-var textAddLeter = document.querySelector("#inputAddleter");
+// capturar click botones
+var buttonAdd = document.querySelector(".buttonAdd").addEventListener("click",(event) => {
+    event.preventDefault();
+    console.log("Click addWord");
+    addNewWords(AddWordsForm);
+});
 
+var buttonAddleter = document.querySelector(".buttonAddleter").addEventListener("click",(event) => {
+    event.preventDefault()
+    console.log("click addleter");
+});
+
+var buttonStart = document.querySelector(".buttonStart").addEventListener("click",(event) => {
+    event.preventDefault();
+    console.log("click Start");
+});
+
+
+// definiendo variables
 let AddedWords = [];
 let preterminedWords = ['javascript', 'html', 'css', 'sublime-text', 'visual-studio', 'variable', 'pagina-web', 'programacion', 'logica', 'oracle-one', 'alura', 'trello']
 let words = { AddedWords, preterminedWords };
-*/
+let text
 
-// capturar click botones
+// agregar nuevas palabras
+function addNewWords(AddWordsForm) {
+    text = AddWordsForm.input.value.toUpperCase();
+    text = text.replace(new RegExp("[àáâãäå\\sæçèéêëìíîïñòóôõöœùúûüýÿ\\W1234567890]", 'g'),"");
 
-buttonAdd.addEventListener('click', function(event) {
-    event.preventDefault();
-    console.log('click buttonAdd');
-});
+    //document.getElementById("inputExit").value = text.toLowerCase();
+    console.log(text);
+}
 
-buttonAddLeter.addEventListener('click', function(event) {
-    event.preventDefault();
-    console.log('click buttonAdd');
-});
+
