2e8a981a5bfb9044465c8dae7218c1163d79980b
JohnDuranM <jhojamudu@gmail.com>
2022-02-09 14:07:49 -0500

    creating sucess and fail logic

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,16 @@
+//funcion remplazo
+//String.prototype.replaceAt=function(index, character) { return this.substr(0, index) + character + this.substr(index+character.length); } 
+
+// definiendo variables
+let AddedWords = [];
+let preterminedWords = ['JAVASCRIPT', 'HTML', 'CSS', 'SUBLIME-TEXT', 'VISUAL-STUDIO', 'VARIABLE', 'WEBPAGE', 'PROGRAMACION', 'LOGICA', 'ORACLE-ONE', 'ALURA', 'TRELLO']
+let text ="";
+let lifes = 8;
+
 // capturar inputs 
 var AddWordsForm = document.querySelector(".newWordForm");
 
 var inputAddLeter = document.querySelector("#InputAddleter");
-
-// definiendo variables
-let AddedWords = [];
-let preterminedWords = ['javascript', 'html', 'css', 'sublime-text', 'visual-studio', 'variable', 'pagina-web', 'programacion', 'logica', 'oracle-one', 'alura', 'trello']
-let text ="";
-let lifes = 5;
 
 //Definiendo la palabra al azar 
 var hideWord = preterminedWords[Math.floor(Math.random()*preterminedWords.length)];
@@ -15,41 +18,93 @@ console.log(hideWord);
 
 //receptor guiones   
 let underscores = hideWord.replace(/./g,"_ ");
-console.log(underscores);
 
 
 // captura click y accion del boton agregar 
 var buttonAdd = document.querySelector(".buttonAdd").addEventListener("click",(event) => {
-    event.preventDefault();
+    //event.preventDefault();
     console.log("Click addWord");
     addNewWords(AddWordsForm);
 });
 
 // captura click y accion del boton comprobar letra
 var buttonAddleter = document.querySelector(".buttonAddleter").addEventListener("click",(event) => {
-    event.preventDefault()
+    //event.preventDefault()
     console.log("click addleter");
     //captura de letra y conversion a mayuscula
-    var leter = document.querySelector('.InputAddleter ').value.toUpperCase();
+    let leter = document.querySelector('.InputAddleter ').value.toUpperCase();
     console.log(leter)
-    //verificar coincidencia (aciertos) letra en palabra
-    for (const i in hideWord){
-      if(leter == hideWord[i]){
-        underscores = underscores.replaceAT(i*2, leter);
+
+    let newWord= "";
+
+    for (let i = 0; i < hideWord.length;i++){
+      if(leter == hideWord[i]) {
+        newWord = newWord + leter + " ";
+      }else{
+        newWord = newWord + underscores[i*2] + " ";
       }
     }
-    console.log(underscores);
+    console.log(lifes);
+    console.log(newWord);
+
+    if(newWord == underscores){
+      lifes--;
+      console.log(lifes)
+    }
+    
+    // definiendo acciones segun intentos restantes 
+    if (lifes == 7) {
+      vidaSiete();
+    }
+      
+    if (lifes == 6) {
+      vidaSeis();
+    }
+
+    if (lifes == 5) {
+      vidaCinco();
+    }
+
+    if (lifes == 4) {
+      vidaCuatro();
+    }
+
+    if (lifes == 3) {
+      vidaTres();
+    }
+
+    if (lifes == 2) {
+      vidaDos();
+    }
+
+    if (lifes == 1) {
+      vidaUno();
+    }
+
+    if (lifes == 0 ) {
+      console.log ("La palabre era : " + hideWord);
+      vidaCero();
+    }
+
+    
+    //verificar coincidencia (aciertos) letra en palabra
+    //for (const i in hideWord){
+      //if(leter == hideWord[i]){
+          //underscores = underscores.replaceAT(i*2, leter);
+      //}
+    //}
+    //alert(underscores);
+
 });
 
 // definiendo accion boton jugar
 var buttonStart = document.querySelector(".buttonStart").addEventListener("click",(event) => {
-    event.preventDefault();
+    //event.preventDefault();
     console.log("click Start");
     hideWord = preterminedWords[Math.floor(Math.random()*preterminedWords.length)]; 
     console.log(hideWord);
     //remplazo letras por guiones en palabra oculta luego de click en boton start
-    underscores = hideWord.replace(/./g,"_ ");
-    console.log(underscores);  
+    underscores = hideWord.replace(/./g,"_ ");  
 });
 
 // funcion para agregar nuevas palabras y validacion de texto 
@@ -62,41 +117,6 @@ function addNewWords(AddWordsForm) {
   }
 
 
-
-
-// definiendo acciones segun intentos restantes 
-if (lifes == 7) {
-  vidaSiete();
-}
-  
-if (lifes == 6) {
-  vidaSeis();
-}
-
-if (lifes == 5) {
-  vidaCinco();
-}
-
-if (lifes == 4) {
-  vidaCuatro();
-}
-
-if (lifes == 3) {
-  vidaTres();
-}
-
-if (lifes == 2) {
-  vidaDos();
-}
-
-if (lifes == 1) {
-  vidaUno();
-}
-
-if (lifes == 0 ) {
-  console.log ("La palabre era : " + hideWord);
-  vidaCero();
-}
 
 //alabraUsuario = nuevaPalabra;
 //document.getElementById("frase").innerHTML = palabraUsuario;
